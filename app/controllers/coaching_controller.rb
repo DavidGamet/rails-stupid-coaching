bd4837d8398919532cbc44b484a26b8210c33908
pc <atelierdufilm@orange.fr>
2017-03-06 14:43:41 +0100

    exo1

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,43 @@
+class CoachingController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @query = params[:query]
+    @coach_answer = coach_answer(@query)
+  end
+
+  # while @answer != ""
+  #  print "> "
+  #   message = gets.chomp
+  #   @answer = coach_answer_enhanced(message)
+  #  end
+
+  def coach_answer(your_message)
+      if your_message.downcase == "i am going to work right now!"
+        ''
+      elsif your_message.end_with?("?")
+        "Silly question, get dressed and go to work!"
+      else
+        "I don't care, get dressed and go to work!"
+      end
+  end
+
+  def coach_answer_enhanced(your_message)
+      @answer = coach_answer(your_message)
+      if answer != ""
+        if your_message.upcase == your_message
+          "I can feel your motivation! #{answer}"
+        else
+          @answer
+        end
+      else
+        ""
+      end
+  end
+end
+
+
+
+
+
